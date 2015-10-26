60148303e57f6c48421409ad8e598aa099f42f6f
stijnana <stijn.olthof@live.nl>
2015-10-26 12:15:10 +0100

    '1.0'

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(0);
+include 'lib/functions.php';
+
+writeHello();
+echo '<br>';
+
+writeMsg('hello');
+echo '<br>';
+
+writeMsg('byebye');
+echo '<br>';
+
+writeMsgTo('stijn','how are you doing');
+echo '<br>';
+
+writeMsgTo('stijn');
+echo '<br>';
+
+cal(2,2);
+echo '<br>';
+
+cal2(4,6);
+echo '<br>';echo '<br>';echo '<br>';
+
+
+body();
+
---
+++ lib/functions.php
@@ -0,0 +1,41 @@
+<?php
+
+
+function writeHello(){
+    echo 'Hello!';
+}
+
+function writeMsg($msg){
+    echo $msg;
+}
+
+function writeMsgTo($name, $smg='Default'){
+    echo 'msg from '.$name.' the msg = '.$smg;
+}
+
+function cal($val1,$val2){
+    $outcome = $val1 + $val2;
+    echo 'The outcome of ' .$val1 .' + '. $val2. '= ' .$outcome;
+}
+
+function cal2($val1,$val2){
+    $outcome = $val1 * $val2;
+    echo 'The outcome of ' .$val1 .'*'. $val2. '= ' .$outcome;
+}
+
+function best($val1,$val2,$sort){
+    $outcome = $val1. $sort. $val2;
+    echo 'The outcome of ' .$val1 .$sort. $val2. '= ' .$outcome;
+}
+
+function body(){
+    include 'lib/form.html';
+    $height = $_POST['height'];
+    $weight = $_POST['weight'];
+    $outcome = $weight / ($height * $height);
+
+    echo $outcome;
+
+
+}
+
